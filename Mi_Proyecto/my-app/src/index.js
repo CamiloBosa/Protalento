09db5bce9f47863d2499d3852e5bff787761f3b7
CamiloBosa <camiloandresbosa@gmail.com>
2024-04-11 11:57:09 -0500

    Testimonios

--- Mi_Proyecto/my-app/src/index.js
+++ Mi_Proyecto/my-app/src/index.js
@@ -1,35 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+//import { Greeting, UserCard } from "./Greeting";
+//import Product, { Navbar } from "./Product";
+import { Button } from "./Button";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Greeting() {
-  const married = false;
-  return(
-  <div>
-    <h1>{married.toString()}</h1>
-  </div>
-    ); 
-  }
-/*
-function Greeting() {
-  const user = {
-    firstName: "Ryan",
-    lastName: "Ray",
-  };
-  return (
-    <div>
-      <h1>{user.firstName}</h1>
-      <h3>{user.lastName}</h3>
-    </div>
-  );
-}
-*/
 root.render(
-  <div>
-    <Greeting />
-    <Greeting />
-    <Greeting />
-    <Greeting />
-  </div>
+  <>
+  <Button/>
+  </> 
 );
