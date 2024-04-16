1368d72d9dba4a2f0f3b6a9cc429249cab213e61
CamiloBosa <camiloandresbosa@gmail.com>
2024-04-16 11:51:17 -0500

    restricciones

--- Nuevo_Proyecto/calculadora/src/App.js
+++ Nuevo_Proyecto/calculadora/src/App.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import logo from './imagenes/imagen.jpg';
 import Boton from './componentes/Boton';
 import Pantalla from './componentes/Pantalla';
 import BotonClear from './componentes/BotonClear';
-import { useState } from 'react';
-import {evaluate} from 'mathjs'
+import { evaluate } from 'mathjs';
 
 function App() {
-
   const [input, setInput] = useState('');
-  const agregarInput = val =>{
-    setInput(input + val) 
-  }
 
-  const calcularResultado = () =>{
-    if (input){
-      setInput(evaluate(input))
-    } else{
-      alert("Por favor ingrese valores para realizar los cálculos")
-    }
+  const agregarInput = val => {
+    const lastChar = input[input.length - 1];
+    const operators = "+-*/";
     
+    if (operators.includes(lastChar) && operators.includes(val)) {
+      return alert("Por favor ingrese valores correctos para realizar los cálculos")
+
+    } else {
+     
+      setInput(input + val);
+    }
   };
 
+  const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert("Por favor ingrese valores para realizar los cálculos");
+    }
+  };
 
   return (
     <div className="App">
@@ -34,7 +40,7 @@ function App() {
         />
       </div>
       <div className='contenedor-calculadora'>
-        <Pantalla  input = {input} />
+        <Pantalla input={input} />
         <div className='fila'>
           <Boton manejarClic={agregarInput}>1</Boton>
           <Boton manejarClic={agregarInput}>2</Boton>
@@ -60,9 +66,7 @@ function App() {
           <Boton manejarClic={agregarInput}>/</Boton>
         </div>
         <div className='fila'>
-          <BotonClear manejarClear ={() => setInput('')}>
-            Clear
-          </BotonClear>
+          <BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
         </div>
       </div>
     </div>
